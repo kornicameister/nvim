63e954f0fd2b96b7e03ed4f4f43e4904dd55243e
kornicameister <kornicameister@gmail.com>
2021-06-11 01:00:18 +0200

    Mainly some initil git setup + reformat

--- init.lua
+++ init.lua
@@ -3,11 +3,10 @@
 -- LIKE A MOTHER-FUCKER
 -- PERSONAL NVIM OF KORNICAMEISTER
 ---
-
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g      -- a table to access global variables
-local opt = vim.opt  -- to set options
+local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g -- a table to access global variables
+local opt = vim.opt -- to set options
 
 local plugins = require('plugins')
 local utils = require('utils')
@@ -28,7 +27,7 @@ nnoremap('<c-j>', '<c-w>j')
 nnoremap('<c-k>', '<c-w>k')
 nnoremap('<c-l>', '<c-w>l')
 -- window resize
-nnoremap('<c-left>',  [[:vertical resize +2<cr>]] )
-nnoremap('<c-right>', [[:vertical resize -2<cr>]] )
-nnoremap('<c-up>',    [[:resize +2<cr>]] )
-nnoremap('<c-down>',  [[:resize -2<cr>]] )
+nnoremap('<c-left>', [[:vertical resize +2<cr>]])
+nnoremap('<c-right>', [[:vertical resize -2<cr>]])
+nnoremap('<c-up>', [[:resize +2<cr>]])
+nnoremap('<c-down>', [[:resize -2<cr>]])
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,34 +1,44 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local packar_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packar_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(packar_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim','--depth',1, packar_path})
-  execute 'packadd packer.nvim'
+    fn.system({
+        'git', 'clone', 'https://github.com/wbthomason/packer.nvim', '--depth',
+        1, packar_path
+    })
+    execute 'packadd packer.nvim'
 end
 
 return require('packer').startup(function(use)
-  -- self manage for packer
-  use 'wbthomason/packer.nvim'
+    -- self manage for packer
+    use 'wbthomason/packer.nvim'
 
-  -- editor enhanced
-  use {
-    'lukas-reineke/indent-blankline.nvim',
-    branch = 'lua',
-    setup = [[require('config.indentline')]]
-  }
+    -- git
+    use {
+        'tpope/vim-fugitive', {
+            'airblade/vim-gitgutter',
+            setup = [[require('config.gitgutter')]]
+        }
+    }
 
-  -- UI
-  use {
-      'dylanaraps/wal.vim',
-      'ryanoasis/vim-devicons',
-      'psliwka/vim-smoothie'
-  }
+    -- editor enhanced
+    use {
+        {
+            'lukas-reineke/indent-blankline.nvim',
+            branch = 'lua',
+            setup = [[require('config.indentline')]]
+        },
+        {
+            'ap/vim-css-color',
+            ft = {'css', 'scss', 'sass', 'elm', 'typescript', 'vue'}
+        }
+    }
 
-  -- other
-  use {
-    'svermeulen/vimpeccable',
-    'wakatime/vim-wakatime'
-  }
+    -- UI
+    use {'dylanaraps/wal.vim', 'ryanoasis/vim-devicons', 'psliwka/vim-smoothie'}
+
+    -- other
+    use {'svermeulen/vimpeccable', 'wakatime/vim-wakatime'}
 
 end)
