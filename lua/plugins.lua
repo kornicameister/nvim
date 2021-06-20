a942de90d857d397ef07078700adfcf3364be77b
kornicameister <kornicameister@gmail.com>
2021-06-20 23:58:29 +0200

    fixup! Reformat a bit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,18 +2,22 @@ local execute = vim.api.nvim_command
 local fn = vim.fn
 local g = vim.g
 
-local packar_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packar_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(packar_path)) > 0 then
-    fn.system({
-        'git', 'clone', 'https://github.com/wbthomason/packer.nvim', '--depth',
-        1, packar_path
-    })
-    execute 'packadd packer.nvim'
+  fn.system({
+    "git",
+    "clone",
+    "https://github.com/wbthomason/packer.nvim",
+    "--depth",
+    1,
+    packar_path,
+  })
+  execute("packadd packer.nvim")
 end
 
-return require('packer').startup(function(use)
-    -- self manage for packer
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+  -- self manage for packer
+  use("wbthomason/packer.nvim")
 
     -- python
     use {
