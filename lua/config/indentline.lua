570f74f4e8bef70c2d783585f09f424be8587660
kornicameister <kornicameister@gmail.com>
2021-06-22 00:38:04 +0200

    Add configuration for indentline

--- lua/config/indentline.lua
+++ lua/config/indentline.lua
@@ -1,8 +1,16 @@
 local g = vim.g
 
-g.indentLine_char = '│'
+g.indent_blankline_char = "┊"
 g.indent_blankline_use_treesitter = true
-g.indentLine_faster = 1
-g.indentLine_fileTypeExclude = {'tex', 'markdown', 'txt', 'startify', 'packer'}
+g.indent_blankline_filetype_exclude = {
+  "tex",
+  "markdown",
+  "txt",
+  "startify",
+  "packer",
+  "fzf",
+  "help",
+}
+g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 g.indent_blankline_show_first_indent_level = false
 g.indent_blankline_show_trailing_blankline_indent = false
