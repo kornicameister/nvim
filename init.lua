d0a47c7961660acaf1d2ed05c56490195dfeff1f
kornicameister <kornicameister@gmail.com>
2021-06-23 01:34:34 +0200

    Disable perl + ruby

--- init.lua
+++ init.lua
@@ -20,6 +20,8 @@ local autocmd = utils.autocmd
 local buffer = { o, bo }
 local window = { o, wo }
 
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
 g.python2_host_prog = env.PYENV_ROOT .. "/versions/neovim2/bin/python"
 g.python3_host_prog = env.PYENV_ROOT .. "/versions/neovim3/bin/python"
 
