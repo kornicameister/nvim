28e5bfc02a9e8f61a3eded864c2c31f26e537d8d
kornicameister <kornicameister@gmail.com>
2021-06-27 23:57:07 +0200

    fixup! Move bufferline config to own module

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -5,9 +5,22 @@ require("bufferline").setup({
     numbers = "buffer_id",
     diagnostics = "nvim_lsp",
     diagnostics_indicator = function(count, level)
-      local icon = level:match('error') and ' ' or ''
-      return ' ' .. icon .. ' ' .. count
+      local icon = level:match("error") and " " or ""
+      return " " .. icon .. " " .. count
     end,
     show_buffer_close_icons = false,
   },
 })
+
+local opts = {
+  noremap = true,
+  silent = true,
+}
+
+vim.api.nvim_set_keymap("n", "<Tab>", "<CMD>BufferLineCycleNext<CR>", opts)
+vim.api.nvim_set_keymap("n", "<S-Tab>", "<CMD>BufferLineCyclePrev<CR>", opts)
+
+vim.api.nvim_set_keymap("n", "<leader>q", [[ :bd<cr>gT ]], opts)
+vim.api.nvim_set_keymap("n", "<leader>Q", [[ :bufdo bd<CR> ]], opts)
+vim.api.nvim_set_keymap("n", "<leader>r", [[ :e<CR> ]], opts)
+vim.api.nvim_set_keymap("n", "<leader>R", [[ :e!<CR> ]], opts)
