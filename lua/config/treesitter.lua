a06a7cf56579f4dfe20a24b92fc6b05bf1323635
kornicameister <kornicameister@gmail.com>
2021-09-11 20:23:45 +0200

    Enable inrecemental selection

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -31,6 +31,12 @@ require("nvim-treesitter.configs").setup({
 
   incremental_selection = {
     enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
   },
 
   matchup = {
