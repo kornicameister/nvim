8dc30ced37d5fcf6c182e5a5fdd070f3ce9173a7
Tomasz Trebski <kornicameister@gmail.com>
2026-03-13 10:38:20 +0100

    chore: update lazy-lock and add typescript lang config

---
+++ lua/lang/typescript.lua
@@ -0,0 +1,71 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = {servers = {vtsls = {}}}
+    --[[
+       [opts = {
+       [  servers = {
+       [    vtsls = {
+       [      -- explicitly add default filetypes, so that we can extend
+       [      -- them in related extras
+       [      filetypes = {
+       [        'javascript',
+       [        'javascriptreact',
+       [        'javascript.jsx',
+       [        'typescript',
+       [        'typescriptreact',
+       [        'typescript.tsx',
+       [      },
+       [      settings = {
+       [        complete_function_calls = true,
+       [        vtsls = {
+       [          enableMoveToFileCodeAction = true,
+       [          autoUseWorkspaceTsdk = true,
+       [          experimental = {
+       [            maxInlayHintLength = 30,
+       [            completion = {
+       [              enableServerSideFuzzyMatch = true,
+       [            },
+       [          },
+       [        },
+       [        typescript = {
+       [          updateImportsOnFileMove = { enabled = 'always' },
+       [          suggest = {
+       [            completeFunctionCalls = true,
+       [          },
+       [          inlayHints = {
+       [            enumMemberValues = { enabled = true },
+       [            functionLikeReturnTypes = { enabled = true },
+       [            parameterNames = { enabled = 'literals' },
+       [            parameterTypes = { enabled = true },
+       [            propertyDeclarationTypes = { enabled = true },
+       [            variableTypes = { enabled = false },
+       [          },
+       [        },
+       [      },
+       [      keys = {
+       [        {
+       [          'gD',
+       [          function()
+       [            vim.lsp.buf.declaration()
+       [          end,
+       [          desc = 'Goto Source Definition',
+       [        },
+       [      },
+       [    },
+       [  },
+       [  setup = {
+       [    vtsls = function(_, opts)
+       [      -- copy typescript settings to javascript
+       [      opts.settings.javascript = vim.tbl_deep_extend(
+       [        'force',
+       [        {},
+       [        opts.settings.typescript,
+       [        opts.settings.javascript or {}
+       [      )
+       [    end,
+       [  },
+       ]]
+    },
+  },
+}
