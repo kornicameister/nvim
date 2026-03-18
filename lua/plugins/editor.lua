c6f70d61514577d4b08c78230d382fdba74f2171
Tomasz Trebski <kornicameister@gmail.com>
2026-03-18 23:28:24 +0100

    feat(editor): add hardtime.nvim to break bad habits
    - shows hints for better motions
    - blocks repeated hjkl spam and arrow keys
    - :Hardtime report to see most common bad habits

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,11 @@
 return {
   {
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {},
+  },
+  {
     'nvim-pack/nvim-spectre',
     build = false,
     cmd = 'Spectre',
