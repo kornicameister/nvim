63ece5d3dba46a530bd76ac079bffe271224307e
Tomasz Trebski <kornicameister@gmail.com>
2026-04-01 13:13:48 +0200

    fix(config): load options before autocmds to restore relativenumber

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,9 +1,9 @@
 vim.loader.enable()
 
+require('config.options')
 require('config.filetypes')
 require('config.autocmds')
 require('config.folds')
 require('config.lazy')
-require('config.options')
 require('config.spell')
 require('config.keymaps')
