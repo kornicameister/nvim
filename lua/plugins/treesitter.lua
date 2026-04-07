8ed5bef1cb4d92d57f94aa9a8faf9df8af98c055
Tomasz Trebski <kornicameister@gmail.com>
2026-04-07 13:44:35 +0200

    feat(treesitter): add incremental selection with native treesitter API
    Use <C-space> to init selection, <space> to expand to parent node,
    <bs> to shrink to child node. Replaces treesitter-modules.nvim which
    had init race condition with lazy.nvim.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,6 +17,34 @@ return {
           end
         end,
       })
+
+      -- incremental selection via treesitter nodes
+      local node_sel = nil
+      local function select_node(node)
+        local sr, sc, er, ec = node:range()
+        -- exit visual first, then reselect charwise
+        local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
+        vim.api.nvim_feedkeys(esc, 'nx', false)
+        vim.api.nvim_win_set_cursor(0, { sr + 1, sc })
+        vim.cmd('normal! v')
+        vim.api.nvim_win_set_cursor(0, { er + 1, ec > 0 and ec - 1 or 0 })
+      end
+      vim.keymap.set('n', '<C-space>', function()
+        node_sel = vim.treesitter.get_node()
+        if node_sel then select_node(node_sel) end
+      end, { desc = 'Init treesitter selection' })
+      vim.keymap.set('x', '<space>', function()
+        if node_sel and node_sel:parent() then
+          node_sel = node_sel:parent()
+          select_node(node_sel)
+        end
+      end, { desc = 'Expand treesitter selection' })
+      vim.keymap.set('x', '<bs>', function()
+        if node_sel and node_sel:child(0) then
+          node_sel = node_sel:child(0)
+          select_node(node_sel)
+        end
+      end, { desc = 'Shrink treesitter selection' })
     end,
     dependencies = { 'hiphish/rainbow-delimiters.nvim', 'windwp/nvim-ts-autotag' },
   },
